316b6ee1b0bf632980fc0eb00384f8877b76acc3
Florian Appel <florian.appel@proton.me>
2023-01-09 16:28:31 +0100

    add content

--- notes/models.py
+++ notes/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
 class Notes(models.Model):
     title = models.CharField(max_length=30)
     note = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=None, blank=True, null=True)
