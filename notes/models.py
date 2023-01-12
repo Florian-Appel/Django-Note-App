10b269b400d3e48e43c99fb98e2a7120087078fe
Florian Appel <florian.appel@proton.me>
2023-01-12 16:57:59 +0100

    change models

--- notes/models.py
+++ notes/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 
-# Create your models here.
 
 class Category(models.Model):
     name = models.CharField(max_length=20)
 
 class Notes(models.Model):
+
+    CATEGORIES = (
+        ("GEN", "General"),
+        ("PRI", "Privat"),
+        ("PRO", "Professional"),
+        ("OTH", "Other"),
+    )
+
     title = models.CharField(max_length=30)
     note = models.CharField(max_length=100)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=None, blank=True, null=True)
+    #category = models.ForeignKey(Category, on_delete=models.CASCADE, default=None, blank=True, null=True)
+    category = models.CharField(max_length=20, choices=CATEGORIES, default=None, blank=True, null=True)
