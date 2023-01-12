1caedfff56d0dfe74304379f4f93316a85d5a952
Florian Appel <florian.appel@proton.me>
2023-01-12 15:59:03 +0100

    change models

--- notes/models.py
+++ notes/models.py
@@ -8,5 +8,4 @@ class Category(models.Model):
 class Notes(models.Model):
     title = models.CharField(max_length=30)
     note = models.CharField(max_length=100)
-    #category = models.ForeignKey(Category, on_delete=models.CASCADE, default=None, blank=True, null=True)
-    category = models.CharField(max_length=20)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=None, blank=True, null=True)
